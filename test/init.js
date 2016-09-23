0e701cc98872d88518f98f5cfb00620b22e15f3a
Simon Ho <superkhau@gmail.com>
2016-09-23 13:34:39 -0700

    Refactor test skipping to detect CI environment (#13)
    - See comments at strongloop/loopback-connector-db2z/pull/10
    - Fix linting errors

--- test/init.js
+++ test/init.js
@@ -1,26 +1,30 @@
-//// Copyright IBM Corp. 2016. All Rights Reserved.
-//// Node module: loopback-connector-db2z
-//// This file is licensed under the Artistic License 2.0.
-//// License text available at https://opensource.org/licenses/Artistic-2.0
-//
-//module.exports = require('should');
-//
-//var DataSource = require('loopback-datasource-juggler').DataSource;
-//
-//var config = {
-//  username: process.env.DB2Z_USERNAME,
-//  password: process.env.DB2Z_PASSWORD,
-//  hostname: process.env.DB2Z_HOSTNAME || 'localhost',
-//  port: process.env.DB2Z_PORTNUM || 60000,
-//  database: process.env.DB2Z_DATABASE || 'testdb',
-//  schema: process.env.DB2Z_SCHEMA || 'STRONGLOOP',
-//};
-//
-//global.config = config;
-//
-//global.getDataSource = global.getSchema = function(options) {
-//  var db = new DataSource(require('../'), config);
-//  return db;
-//};
-//
-//global.sinon = require('sinon');
+// Copyright IBM Corp. 2016. All Rights Reserved.
+// Node module: loopback-connector-db2z
+// This file is licensed under the Artistic License 2.0.
+// License text available at https://opensource.org/licenses/Artistic-2.0
+
+'use strict';
+
+var describe = require('./describe');
+
+module.exports = require('should');
+
+var DataSource = require('loopback-datasource-juggler').DataSource;
+
+var config = {
+  username: process.env.DB2Z_USERNAME,
+  password: process.env.DB2Z_PASSWORD,
+  hostname: process.env.DB2Z_HOSTNAME || 'localhost',
+  port: process.env.DB2Z_PORTNUM || 60000,
+  database: process.env.DB2Z_DATABASE || 'testdb',
+  schema: process.env.DB2Z_SCHEMA || 'STRONGLOOP',
+};
+
+global.config = config;
+
+global.getDataSource = global.getSchema = function(options) {
+  var db = new DataSource(require('../'), config);
+  return db;
+};
+
+global.sinon = require('sinon');
